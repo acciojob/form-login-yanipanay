b6a76f8e72d22b297d29a6181f6f80ea80fc584d
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-10-11 01:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
+const button = document.querySelector('[type="submit"]');
 
+button.addEventListener('click',getFormvalue);
+
+
+function getFormvalue(event) {
+    //Write your code here
+	event.preventDefault();
+	const fname = document.querySelector('[name="fname"]').value;
+	const lname = document.querySelector('[name="lname"]').value;
+
+	alert(fname+" "+lname);
 }
